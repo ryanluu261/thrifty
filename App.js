8a6035cf898ae956865d54b772adc1b78d0880f6
fpoon777 <fpoon777@gmail.com>
2022-05-18 16:26:24 -0400

    finished ecsa for react native

--- App.js
+++ App.js
@@ -1,21 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import { LogBox } from 'react-native';
+import MainTabBar from './navigation/main_tab_bar';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar />
-    </View>
-  );
+LogBox.ignoreAllLogs();
+
+function App(props) {
+  return <MainTabBar />;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
